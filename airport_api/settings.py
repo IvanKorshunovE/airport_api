23e7793c0daabc906be3e68d8e0c605a2759a9fa
Ivan <ivan.korshunov.1997@gmail.com>
2023-08-02 23:00:04 +0300

    Implement Tokenization using JWT.

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "debug_toolbar",
     "drf_spectacular",
+    "rest_framework_simplejwt",
     "routes",
     "flights",
     "orders",
@@ -107,6 +108,9 @@ AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
+        )
 }
 
 SPECTACULAR_SETTINGS = {
--- airport_api/urls.py
+++ airport_api/urls.py
@@ -7,12 +7,15 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView
 )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("routes/", include("routes.urls", namespace="routes")),
     path("flights/", include("flights.urls", namespace="flights")),
     path("orders/", include("orders.urls", namespace="orders")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
